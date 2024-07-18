81de24687100255fc9524b40ec84c63608f006f9
Najoan <68422289+najoan125@users.noreply.github.com>
2024-07-18 11:53:23 +0900

    Update kill.js

--- src/renderer/actions/utils/kill.js
+++ src/renderer/actions/utils/kill.js
@@ -12,11 +12,11 @@ export default async function killProcesses(channels, progressPerLoop, shouldRes
         if (process.platform === "darwin") processName = platforms[channel]; // Discord Canary and Discord PTB on Mac
         else processName = platforms[channel].replace(" ", ""); // DiscordCanary and DiscordPTB on Windows/Linux
 
-        log("Attempting to kill " + processName);
+        log(processName + " 종료 시도 중");
         try {
             const results = await findProcess("name", processName, true);
             if (!results || !results.length) {
-                log(`✅ ${processName} not running`);
+                log(`✅ ${processName} 이(가) 실행 중이지 않습니다`);
                 progress.set(progress.value + progressPerLoop);
                 continue;
             }
@@ -30,7 +30,7 @@ export default async function killProcesses(channels, progressPerLoop, shouldRes
         }
         catch (err) {
             const symbol = shouldRestart ? "⚠️" : "❌";
-            log(`${symbol} Could not kill ${platforms[channel]}`);
+            log(`${symbol} ${platforms[channel]}을(를) 종료할 수 없습니다`);
             log(`${symbol} ${err.message}`);
             return err;
         }

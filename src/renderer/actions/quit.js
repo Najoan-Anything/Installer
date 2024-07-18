efca1e143e75e62fc5701039ca06ae677f97ba3b
Najoan <68422289+najoan125@users.noreply.github.com>
2024-07-18 11:28:25 +0900

    Update quit.js

--- src/renderer/actions/quit.js
+++ src/renderer/actions/quit.js
@@ -2,11 +2,11 @@ import {remote} from "electron";
 export default async function() {
     const confirmation = await remote.dialog.showMessageBox(remote.BrowserWindow.getFocusedWindow(), {
         type: "question",
-        title: "Are you sure?",
-        message: "Are you sure you want to quit the installation?",
+        title: "확실합니까?",
+        message: "설치를 종료하시겠습니까?",
         noLink: true,
         cancelId: 1,
-        buttons: ["Quit", "Cancel"]
+        buttons: ["종료", "취소"]
     });
 
     if (confirmation.response === 0) {
